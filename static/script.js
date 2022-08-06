5c520cdf48588d6e74420c18ceb3886240273071
MarinaASUS <marinabar1609@gmail.com>
2022-08-06 19:27:27 +0300

    added footer

--- static/script.js
+++ static/script.js
@@ -16,7 +16,10 @@ function clouds() {
 function piz() {
     img = document.getElementById("mainimg")
     nombre = Math.floor(Math.random() * 10);
-    img.src =`../static/images/pizza${nombre}.png`
-    img.style ="height:30vh; border: 1vh solid black; box-shadow: 3vh 3vh yellow, -3vh -3vh white, 3.5vh 3.5vh black, 4.5vh 4.5vh red"
-    document.getElementById("floating").style.visibility= "visible"
+    img.src =`../static/images/pizza${nombre}.png`;
+    img.style.height = "30vh";
+    img.style.border = '1vh solid black';
+    img.style.boxShadow = "3vh 3vh yellow, -3vh -3vh white, 3.5vh 3.5vh black, 4.5vh 4.5vh red";
+    img.style.marginBottom = "7vh"
+    document.getElementById("floating").style.visibility= "visible";
   }
