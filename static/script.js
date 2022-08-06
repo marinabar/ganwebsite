230251fbab1146236ed6bd25a0eb0c91c7f04f08
MarinaASUS <marinabar1609@gmail.com>
2022-08-06 21:00:27 +0300

    centralize js function + small design fixes

--- static/script.js
+++ static/script.js
@@ -9,17 +9,24 @@ function imp() {
   }
 
 function clouds() {
-    img = document.getElementById("mainimg")
-    img.src ="https://www.w3schools.com/js/pic_bulboff.gif"
+    img = document.getElementById("mainimg");
+    nombre = Math.floor(Math.random() * 23);
+    img.src =`../static/images/nua${nombre}.png`;
+    image(img)
   }
 
 function piz() {
     img = document.getElementById("mainimg")
     nombre = Math.floor(Math.random() * 10);
     img.src =`../static/images/pizza${nombre}.png`;
-    img.style.height = "30vh";
-    img.style.border = '1vh solid black';
-    img.style.boxShadow = "3vh 3vh yellow, -3vh -3vh white, 3.5vh 3.5vh black, 4.5vh 4.5vh red";
-    img.style.marginBottom = "7vh"
-    document.getElementById("floating").style.visibility= "visible";
+    image(img) 
   }
+
+  // function that changes images caracteristics
+function image(img) {
+    img.style.border = '1vh solid black';
+    img.style.height = "30vh";
+    img.style.boxShadow = "3vh 3vh yellow, -3vh -3vh white, 3.5vh 3.5vh black, 4.5vh 4.5vh red";
+    img.style.marginBottom = "7vh";
+    document.getElementById("floating").style.visibility = "visible";
+}
